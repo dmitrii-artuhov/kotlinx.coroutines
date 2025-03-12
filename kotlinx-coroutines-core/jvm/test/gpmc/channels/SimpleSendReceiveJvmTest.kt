e3af56e43a14de5f5bae64aa86f86f15ae3e5976
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-03-12 15:11:07 +0100

    Update SimpleSendReceiveJvmTest

--- kotlinx-coroutines-core/jvm/test/gpmc/channels/SimpleSendReceiveJvmTest.kt
+++ kotlinx-coroutines-core/jvm/test/gpmc/channels/SimpleSendReceiveJvmTest.kt
@@ -7,35 +7,40 @@ import org.jetbrains.kotlinx.lincheck.*
 import org.junit.Test
 import org.junit.runner.*
 import org.junit.runners.*
+import java.util.concurrent.*
 import kotlin.test.*
 
+/**
+ * Would hang if [Dispatchers.Default] was used.
+ */
 class SimpleSendReceiveJvmTestModelChecking : GPMCTestBase() {
     private val kind: TestChannelKind = TestChannelKind.RENDEZVOUS
-    val n: Int = 2
+    val n: Int = 2 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 1000
     val concurrent: Boolean = true
 
-    @Ignore("Hangs and throws multiple `ThreadAbortedError`s")
     @Test
-    fun testSimpleSendReceive() = runGPMCTest(1) {
-        val channel = kind.create<Int>()
+    fun testSimpleSendReceive() = runGPMCTest(100) {
+        Executors.newFixedThreadPool(2).asCoroutineDispatcher().use { pool ->
+            val channel = kind.create<Int>()
 
-        runBlocking {
-            val ctx = if (concurrent) Dispatchers.Default else coroutineContext
-            launch(ctx) {
-                repeat(n) { channel.send(it) }
-                channel.close()
-            }
-            var expected = 0
-            for (x in channel) {
-                if (!kind.isConflated) {
-                    assertEquals(expected++, x)
-                } else {
-                    assertTrue(x >= expected)
-                    expected = x + 1
+            runBlocking(pool) {
+                //val ctx = if (concurrent) Dispatchers.Default else coroutineContext
+                launch(/*ctx*/ pool) {
+                    repeat(n) { channel.send(it) }
+                    channel.close()
                 }
-            }
-            if (!kind.isConflated) {
-                assertEquals(n, expected)
+                var expected = 0
+                for (x in channel) {
+                    if (!kind.isConflated) {
+                        assertEquals(expected++, x)
+                    } else {
+                        assertTrue(x >= expected)
+                        expected = x + 1
+                    }
+                }
+                if (!kind.isConflated) {
+                    assertEquals(n, expected)
+                }
             }
         }
     }

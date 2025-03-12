bd95f5bc88bf652272b4beeb7a45a41337a01023
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-03-12 15:07:05 +0100

    Update DoubleChannelCloseModelCheckingTest

--- kotlinx-coroutines-core/jvm/test/gpmc/channels/DoubleChannelCloseModelCheckingTest.kt
+++ kotlinx-coroutines-core/jvm/test/gpmc/channels/DoubleChannelCloseModelCheckingTest.kt
@@ -3,23 +3,30 @@ package kotlinx.coroutines.channels
 import gpmc.*
 import kotlinx.coroutines.*
 import org.junit.*
+import java.util.concurrent.*
 
+/**
+ * Would hang if [GlobalScope] was used.
+ */
 class DoubleChannelCloseModelCheckingTest : GPMCTestBase() {
 
-    @Ignore("Hangs")
     @Test
-    fun testDoubleCloseStress() = runGPMCTest(1) {
-        val actor: SendChannel<Int> = GlobalScope.actor<Int>(CoroutineName("actor"), start = CoroutineStart.LAZY) {
-            // empty -- just closes channel
-        }
-        val sender = GlobalScope.launch(CoroutineName("sender")) {
-            try {
-                actor.send(1)
-            } catch (e: ClosedSendChannelException) {
-                // ok -- closed before send
+    fun testDoubleCloseModelChecking() = runGPMCTest(100) {
+        Executors.newFixedThreadPool(2).asCoroutineDispatcher().use { pool ->
+            runBlocking(pool) {
+                val actor: SendChannel<Int> = /*GlobalScope*/ actor<Int>(pool + CoroutineName("actor"), start = CoroutineStart.LAZY) {
+                    // empty -- just closes channel
+                }
+                val sender = /*GlobalScope.*/ launch(pool + CoroutineName("sender")) {
+                    try {
+                        actor.send(1)
+                    } catch (e: ClosedSendChannelException) {
+                        // ok -- closed before send
+                    }
+                }
+                Thread.sleep(1)
+                actor.close()
             }
         }
-        //Thread.sleep(1)
-        actor.close()
     }
 }
